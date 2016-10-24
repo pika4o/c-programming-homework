57b0ad1d5979bf4563f10f375e625a9d564f2488
pika4o <ninovkj@gmail.com>
2016-10-24 21:00:18 +0300

    Update 01_SumnAverage.c

--- G/03/15/01_SumnAverage.c
+++ G/03/15/01_SumnAverage.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
     int main () {
     int n,m,suma,i;
-    float average=0;
+    float average;
     scanf("%d", &n);
     suma=0;
     for (i=0; i<n; i++)
@@ -10,7 +10,7 @@
         scanf("%d", &m);
         suma=suma+m;
     }
-    average=suma/n;
+    average=(float)suma/n;
     printf("\nSum: %d", suma);
     printf("\nAvg: %.2f", average);
 
