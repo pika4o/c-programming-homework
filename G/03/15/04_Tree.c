fb35c26cd7673a033f8a23db79297cce05ee1b07
pika4o <ninovkj@gmail.com>
2016-10-26 17:46:14 +0300

    Update 04_Tree.c

--- G/03/15/04_Tree.c
+++ G/03/15/04_Tree.c
@@ -1,13 +1,12 @@
 
 #include <stdio.h>
-int main()
-{
-int m,i,k,ni;
-scanf("%d",&m);
-ni=0;
+int main() {
+
+int m,i,k,ni=0;
+scanf("%d", &m);
 for(i=1;k<m-1;i++)
 for(k=0;k<i*2-1;k++)ni++;
-printf("%d\n",ni);
+printf("%d\n", ni);
 for(k=0;k<m-1;k++) printf(" ");
 printf("*");
 for(k=0;i<m-1;k++) printf(" ");
@@ -22,5 +21,6 @@ for(k=m-i-1;k!=0;k--) printf(" ");
 printf("\n");
 }
 for(k=0;k<m*2-1;k++) printf("*");
+  
 return 0;
 }
